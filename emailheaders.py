18309004ffec74c07de1f336914c347d25738a54
Yuki Cruz <yuki.cruz@gmail.com>
2018-12-27 23:54:41 -0800

    create CSV for headers

--- emailheaders.py
+++ emailheaders.py
@@ -1,4 +1,5 @@
 import win32com.client
+import csv
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 inbox = outlook.GetDefaultFolder(6)
@@ -7,3 +8,7 @@ message = messages.GetLast()
 mess = message.Body
 internet_header = message.PropertyAccessor.GetProperty("http://schemas.microsoft.com/mapi/proptag/0x007D001F")
 print(internet_header)
+
+with open('headers.csv', 'w', newline='') as csvfile:
+    header_writer = csv.writer(csvfile, delimiter='\t', quotechar='|')
+    header_writer.writerow(internet_header)
